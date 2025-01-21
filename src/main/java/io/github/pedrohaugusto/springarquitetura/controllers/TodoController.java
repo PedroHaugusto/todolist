3a187bdbcd12e2f694d7cc3c2b423e043aefac7d
Pedro Henrique <pedrohaugusto12@gmail.com>
2025-01-21 14:58:53 -0300

    :construction: feat:Criando Put para atualizar status e Get para buscar por ID

--- src/main/java/io/github/pedrohaugusto/springarquitetura/controllers/TodoController.java
+++ src/main/java/io/github/pedrohaugusto/springarquitetura/controllers/TodoController.java
@@ -3,13 +3,15 @@ package io.github.pedrohaugusto.springarquitetura.controllers;
 import io.github.pedrohaugusto.springarquitetura.models.TodoEntity;
 import io.github.pedrohaugusto.springarquitetura.services.TodoService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("todos")
 public class TodoController {
+
+
 
     private TodoService service;
 
@@ -20,5 +22,16 @@ public class TodoController {
     @PostMapping
     public TodoEntity salvar(@RequestBody TodoEntity todo){
         return this.service.salvar(todo);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizarStatus(@PathVariable("id") UUID id, @RequestBody TodoEntity todo){
+        todo.setId(id);
+        service.atualizarStatus(todo);
+    }
+
+    @GetMapping("/{id}")
+    public TodoEntity buscarPorId(@PathVariable("id") UUID id){
+        return service.buscarPorId(id);
     }
 }
--- src/main/java/io/github/pedrohaugusto/springarquitetura/services/TodoService.java
+++ src/main/java/io/github/pedrohaugusto/springarquitetura/services/TodoService.java
@@ -3,6 +3,10 @@ package io.github.pedrohaugusto.springarquitetura.services;
 import io.github.pedrohaugusto.springarquitetura.models.TodoEntity;
 import io.github.pedrohaugusto.springarquitetura.repositories.TodoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 @Service
 public class TodoService {
@@ -15,5 +19,13 @@ public class TodoService {
 
     public TodoEntity salvar(TodoEntity novoTodo) {
         return repository.save(novoTodo);
+    }
+
+    public void atualizarStatus(TodoEntity todo){
+        repository.save(todo);
+    }
+
+    public TodoEntity buscarPorId(UUID id){
+        return repository.findById(id).orElse(null);
     }
 }
